5c9473c583df65542c6fe06812afcdf7d57d1b3c
swj7563 <swj7563@naver.com>
2025-02-13 22:56:47 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+
+playButton.addEventListener("click", play)
+
+function pickRandomNum() {
+    computerNum = Math.floor(Math.random()*100)+1;
+    console.log("정답", computerNum)
+}
+
+function play() {
+    let userValue = userInput.value;
+    if (userValue < computerNum) {
+        resultArea.textContent = "UP!!!"
+    } else if (userValue > computerNum) {
+        resultArea.textContent = "DOWN!!!"
+    } else {
+        resultArea.textContent = "맞추셨습니다!!!"
+    }
+}
+
+pickRandomNum();
